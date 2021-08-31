b42c041d38ffb6b2d8b118385b19285beb1a51dd
DominicAudibert <da01665@srcs.org>
2021-08-31 17:09:13 -0500

    Activity 4 Complete

--- Crab.java
+++ Crab.java
@@ -7,8 +7,62 @@ public class Crab extends Actor
 {
     public void act()
     {
-        // Add your action code here
+         move(3);
+         turnAtEdge();
+         checkKeyPress();
+         onCollision();
     }
+    
+    private void turnAtEdge()
+    {
+        if(isAtEdge())
+        {
+        turn(50);
+        }
+    }
+    
+    private void checkKeyPress()
+        {
+        if(Greenfoot.isKeyDown("up"))
+        {
+            Greenfoot.delay(60);
+        }
+    
+        if(Greenfoot.isKeyDown("right"))
+        {
+            turn(4);
+        }
+        
+        if(Greenfoot.isKeyDown("left"))
+        {
+            turn (-4);
+        }
+        
+    }
+        private void onCollision()
+    {
+        if(isTouching(worming.class))
+        {
+            removeTouching(worming.class);
+            Greenfoot.playSound("slurp.wav");
+        }
+        
+        if(isTouching(Lobstering.class))
+        {
+            
+            Greenfoot.playSound("au.wav");
+            Greenfoot.stop();
+        }
+    }
+        
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
 
--- CrabWorld.java
+++ CrabWorld.java
@@ -11,6 +11,7 @@ public class CrabWorld extends World
         super(560, 560, 1);
         prepare();
     }
+    
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
@@ -18,6 +19,41 @@ public class CrabWorld extends World
     private void prepare()
     {
         Crab crab = new Crab();
-        addObject(crab,300,178);
+        addObject(crab,372,228);
+        Lobstering lobstering = new Lobstering();
+        addObject(lobstering,120,115);
+        worming worming = new worming();
+        addObject(worming,422,375);
+        worming.setLocation(423,372);
+        worming worming2 = new worming();
+        addObject(worming2,423,372);
+        worming worming3 = new worming();
+        addObject(worming3,225,396);
+        worming worming4 = new worming();
+        addObject(worming4,324,468);
+        worming worming5 = new worming();
+        addObject(worming5,100,477);
+        worming worming6 = new worming();
+        addObject(worming6,101,348);
+        worming worming7 = new worming();
+        addObject(worming7,206,296);
+        worming worming8 = new worming();
+        addObject(worming8,79,237);
+        worming worming9 = new worming();
+        addObject(worming9,240,169);
+        worming worming10 = new worming();
+        addObject(worming10,288,57);
+        worming worming11 = new worming();
+        addObject(worming11,499,50);
+        worming worming12 = new worming();
+        addObject(worming12,373,118);
+        worming worming13 = new worming();
+        addObject(worming13,483,175);
+        worming worming14 = new worming();
+        addObject(worming14,492,515);
+        worming worming15 = new worming();
+        addObject(worming15,332,326);
+        worming worming16 = new worming();
+        addObject(worming16,491,297);
     }
 }
